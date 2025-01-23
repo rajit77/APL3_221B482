17b6a6227187e61cae8f17c532daef76d2267c5c
221B482 <221B482@juet.co.in>
2025-01-23 15:31:22 +0530

    this is my third commit

---
+++ LE1/PROB1/Child.java
@@ -0,0 +1,5 @@
+class Child extends Mother{
+    static void show(){
+        System.out.println("I am the show of child class.");
+    }
+}
---
+++ LE1/PROB1/Mother.java
@@ -0,0 +1,9 @@
+class Mother
+{
+    int x;
+    static void show(){
+        System.out.println("I am the show of mother class.");
+    }
+    
+    
+}
