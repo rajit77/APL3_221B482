e2a2ff18197bdfb3ee9c1bf2667bd696422944a0
221B482 <221B482@juet.co.in>
2025-03-27 14:27:01 +0530

    this is my eight commit

--- Template Method/Beverage.java
+++ Template Method/Beverage.java
@@ -4,8 +4,7 @@ abstract class Beverage{
        System.out.println("Pour "+qty+"ml of beverage");
     }
     protected abstract void addCondiment();
-   protected  void stir(){} // Declaring it as private result in no inheritance and in whisky class if this method is defined with an acesss 
-   // specifier as protected then it will be a complete new method and will not be called in main method as main is an outside class.
+   protected  void stir(){} 
     private void serve()
     {
         System.out.println("Serve through waiter");
